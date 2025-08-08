9396b959b83c2c41b040697eceb51cc5c9cbea3b
estudiante <estudiante@uceva.local>
2025-08-08 11:34:14 -0500

    camara prueba 2

--- scripts/camara_control.gd
+++ scripts/camara_control.gd
@@ -1,5 +1,18 @@
 extends Node3D
 
+@export var sensivilidad: float = 0.01
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotation.y += event.relative.x * 0.005
+		get_parent().get_parent().rotate_y(deg_to_rad(- event.relative.x *sensivilidad))
+		rotate_x(deg_to_rad(- event.relative.y*sensivilidad))
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,20 @@
 extends CharacterBody3D
 @export var speed: float = 5.0
 @export var jump: float = 5
+@export var sensivilidad: float = 0.001
+@onready var cam_Pivo = $CamaraPivote
+
 
 func _physics_process(delta: float) -> void:
+	movimiento(delta)
+	move_and_slide()
+	
+
+func _process(delta: float) -> void:
+	pass
+
+
+func movimiento(delta: float):
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 1.5
 	if Input.is_action_just_pressed("saltar") and is_on_floor():
@@ -16,8 +28,10 @@ func _physics_process(delta: float) -> void:
 	else :
 		velocity.x = move_toward(velocity.x,0,speed)
 		velocity.z = move_toward(velocity.z,0,speed)
-		
-	move_and_slide()
 
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		cam_Pivo.rotate_z(deg_to_rad( -event.relative.x * sensivilidad))
+		cam_Pivo.rotate_x(deg_to_rad( -event.relative.y * sensivilidad))
+		
+		
