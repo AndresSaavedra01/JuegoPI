22dc3d92b9e7a0f7cc5d54d03e36f0e0bb5b1cbb
Sebastian Arenas <jose.arenas01@uceva.edu.co>
2025-10-28 22:30:20 -0500

    feature: Diseño preliminar y cambio entre escenas

---
+++ esenas/MenuGUI/menu_Opciones.gd
@@ -0,0 +1,63 @@
+extends Control
+
+@export var option_resolution: OptionButton
+@export var option_fullscreen: CheckBox
+@export var option_borderless: CheckBox
+@export var option_vsync: CheckBox
+@export var back_button: Button
+
+func _ready():
+	var resolutions = [
+		Vector2i(1920,1080),
+		Vector2i(1600,900),
+		Vector2i(1280,720),
+		Vector2i(640,360),
+		Vector2i(320,180),
+	]
+	for res in resolutions:
+		option_resolution.add_item("%dx%d" % [res.x, res.y])
+		
+	load_current_settings()
+	
+	option_resolution.item_selected.connect(_on_resolution_selected)
+	option_fullscreen.toggled.connect(_on_fullscreen_toggled)
+	option_borderless.toggled.connect(_on_borderless_toggled)
+	option_vsync.toggled.connect(_on_vsync_toggled)
+	back_button.pressed.connect(_on_back_pressed)
+
+	
+func load_current_settings():
+	var mode = DisplayServer.window_get_mode()
+	option_fullscreen.button_pressed = mode == DisplayServer.WINDOW_MODE_FULLSCREEN
+	option_borderless.button_pressed = DisplayServer.window_get_flag(DisplayServer.WINDOW_FLAG_BORDERLESS)
+	option_vsync.button_pressed = DisplayServer.window_get_vsync_mode() == DisplayServer.VSYNC_ENABLED
+	
+	var window_size = DisplayServer.window_get_size()
+	for i in range(option_resolution.item_count):
+		var res_text = option_resolution.get_item_text(i)
+		var parts = res_text.split("x")
+		if parts.size() == 2 and int(parts[0]) == window_size.x and int(parts[1]) == window_size.y:
+			option_resolution.select(i)
+			break
+	
+func _on_resolution_selected(index: int):
+	var text = option_resolution.get_item_text(index)
+	var parts = text.split("x")
+	if parts.size() == 2:
+		DisplayServer.window_set_size(Vector2i(int(parts[0]), int(parts[1])))
+		
+func _on_fullscreen_toggled(enabled: bool):
+	if enabled:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		
+func _on_borderless_toggled(enabled: bool):
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, enabled)
+	
+func _on_vsync_toggled(enabled: bool):
+	var mode = DisplayServer.VSYNC_ENABLED if enabled else DisplayServer.VSYNC_DISABLED
+	DisplayServer.window_set_vsync_mode(mode)
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://esenas/MenuGUI/MenuPrincipal.tscn")
--- esenas/MenuGUI/menu_principal.gd
+++ esenas/MenuGUI/menu_principal.gd
@@ -1,8 +1,21 @@
 extends Control
 
+@export var play_button: Button
+@export var settings_button: Button
+@export var exit_button: Button
+
+
 func _ready():
-	%Play.pressed.connect(play)
+	play_button.pressed.connect(_on_play_button_pressed)
+	settings_button.pressed.connect(_on_settings_button_pressed)
+	exit_button.pressed.connect(_on_exit_pressed)
 	
-func play():
+	
+func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://esenas/Level-1.tscn")
 	
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://esenas/MenuGUI/MenuOpciones.tscn")
+	
+func _on_exit_pressed():
+	get_tree().quit()
