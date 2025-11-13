ec8e908aa7445a13cdba6db43973f98367d2a1c8
Sebastian Arenas <jose.arenas01@uceva.edu.co>
2025-11-12 21:51:18 -0500

    fix: cambios en el menu de configuración

--- esenas/MenuGUI/video_settings.gd
+++ esenas/MenuGUI/video_settings.gd
@@ -7,13 +7,13 @@ extends VBoxContainer
 @export var back_button: Button
 
 func _ready():
-	var animator = ButtonAnimator.new()
-	add_child(animator)
-	
-	var buttons: Array = animator.get_all_buttons(self)
-	await get_tree().process_frame
-	buttons.sort_custom(animator.buttons_array_sorting)
-	await animator.animate_buttons(buttons.duplicate(),true,0.16, Vector2(-20,0), Vector2.ZERO, 0.5)
+	#var animator = ButtonAnimator.new()
+	#add_child(animator)
+	#
+	#var buttons: Array = animator.get_all_buttons(self)
+	#await get_tree().process_frame
+	#buttons.sort_custom(animator.buttons_array_sorting)
+	#await animator.animate_buttons(buttons.duplicate(),true,0.16, Vector2(-20,0), Vector2.ZERO, 0.5)
 
 
 	var resolutions = [
